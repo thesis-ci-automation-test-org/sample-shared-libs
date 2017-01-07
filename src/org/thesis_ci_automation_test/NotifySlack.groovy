2481f79c7eeced23fd98f7b797b1cf25623e2467
Mikko Piuhola <mikko.piuhola@digia.com>
2017-01-07 15:21:50 +0000

    Initial Slack implementation

--- src/org/thesis_ci_automation_test/NotifySlack.groovy
+++ src/org/thesis_ci_automation_test/NotifySlack.groovy
@@ -1,8 +1,28 @@
 #!/usr/bin/env groovy
 package org.thesis_ci_automation_test
+import org.foo.GitHelper
 
-static def getNumber() {
-  return 3
+/**
+ * Notify Slack about build results
+ * @param build Current build
+ * @param result Result string TODO: Use actual result enum
+ */
+@NonCPS
+static def notify(build, result = 'FAILURE') {
+    def msg = "${build.getFullDisplayName()} - Build failed!"
+    def color = "danger"
+    if (result == 'SUCCESS') {
+        msg = "${build.getFullDisplayName()} - Build successful"
+        color = "good"
+    }
+
+    msg += " (<${env.BUILD_URL}|Open>)"
+
+    msg += "\nTest Status:\n"
+    msg += "Passed: TODO, Failed: TODO, Skipped: TODO"
+
+    slackSend color: color, message: msg
+    slackSend color: color, message: GitHelper.getChangeLogString(build)
 }
 
 return this
