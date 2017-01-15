e78d7a03d4aee0a936072b9dc146a4cd8e9d376b
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-01-15 19:31:54 +0200

    Add debug to input passing

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -113,6 +113,8 @@ def call(body) {
             input 'Deploy to production?'
           }
 
+          echo 'DEBUG: Passed input step, reaching milestone 5'
+
           // When a milestone is passed, no currently running
           // other job can pass the same milestone,
           // and will be cancelled.
